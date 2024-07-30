dd34723a050149d864428620cc988ed009ef5e0f
Mohamed Abdelaziz <melhosary016@gmail.com>
2024-07-30 19:18:19 +0200

    initial commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -50,7 +50,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteMe = catchAsync(async (req, res, next) => {
-  await User.findByIdAndUpdate(req.user.id, { active: false });
+  await User.findByIdAndDelete(req.user.id);
 
   res.status(204).json({
     status: "success",
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dbURL =
 'mongodb+srv://elhosary:nodejs@cluster0.vptdvxy.mongodb.net/';
   //'mongodb+srv://a7med3del1973:nodejs123@cluster0.gkjci2o.mongodb.net/TukRide';
  
-  
+
 mongoose
   .connect(dbURL, {
     useNewUrlParser: true,
@@ -14,7 +14,7 @@ mongoose
   .then(() => console.log('DB connection successful !'))
   .catch((err) => console.log('DB connection error : ', err));
 
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT} ..`);
 });
